3ca6ad35d36a068859dd00dbcc75e63436002b2a
robin-sitohang <rbnsitohang10@gmail.com>
2023-10-20 14:49:13 +0700

    delete unusage code

--- cypress.config.js
+++ cypress.config.js
@@ -1,13 +1,3 @@
-// const { defineConfig } = require("cypress");
-
-// module.exports = defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-//       // implement node event listeners here
-//     },
-//   },
-// });
-
 const { defineConfig } = require("cypress");
 const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
 const addCucumberPreprocessorPlugin = require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
